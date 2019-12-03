1ccfe97beab49b1720007e1c390b614f424bdf6d
StaZarycki <stazarycki@gmail.com>
2019-12-03 23:14:39 +0100

    Server Update

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/client'));
-app.listen(3000);
+app.listen(3000, () => {
+	console.log("Server started at port 3000");
+});
